4ba18160f90d7376f2ea56c116bdae66a7d1daf3
woneyhoney <woneyhoney@ajou.ac.kr>
2019-07-23 17:53:36 +0900

    flexDirection

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>1</Text>
-        <Text>2</Text>
-        <Text>3</Text>
+        <Text style={styles.text}>1</Text>
+        <Text style={styles.text}>2</Text>
+        <Text style={styles.text}>3</Text>
       </View>
     );
   }
@@ -22,7 +22,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
+  text: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'red',
 
 
-
+  }
 });
